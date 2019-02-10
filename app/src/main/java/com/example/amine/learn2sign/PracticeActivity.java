59f6c2b9ea2ff761a87d6f5d8bba0f181bcc51a5
Sashank Tungaturthi <devil.sas.007@gmail.com>
2019-02-09 19:36:49 -0700

    added practice activity

---
+++ app/src/main/java/com/example/amine/learn2sign/PracticeActivity.java
@@ -0,0 +1,12 @@
+package com.example.amine.learn2sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class PracticeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
